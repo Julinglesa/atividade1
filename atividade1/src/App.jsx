14e60a113c5700595b5923680b7347bb70c65937
Juliana Portella <julinglesaportella@gmai.com>
2024-02-25 17:21:08 -0300

    primeiro commit

---
+++ atividade1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./Componentes/Header/Header.jsx"
+
+
+function App() {
+return(
+  <>
+  <h1>O curso de front-end da vai na web é show!!</h1>
+  </>
+)
+}
+
+export default App
---
+++ atividade1/src/Componentes/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+
+    <header>
+        <h2>atenção</h2>
+    </header>
+    
+    )
+
+}
+
+export default Header
